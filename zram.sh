ca24aa822ea1c0b7931cf30e835562beb646c3ec
Alberto <52979095+generaleramon@users.noreply.github.com>
2019-09-30 18:35:40 +0200

    Update zram.sh
    Static core number and total swap size

--- zram.sh
+++ zram.sh
@@ -1,17 +1,46 @@
 #!/bin/bash
-cores=$(nproc --all)
-modprobe zram
 
+export LANG=C
+
+cores=4
+
+## Disable Zram
+swapoff /dev/zram0
+swapoff /dev/zram1
+swapoff /dev/zram2
+swapoff /dev/zram3
+
+## Remove All SWAP+ZRAM
+if [[ -n $(lsmod | grep zram) ]]; then
+rmmod zram
+fi
+if [[ $1 == stop ]]; then
+exit 0
+fi
 swapoff -a
 
-totalmem=`free | grep -e "^Mem:" | awk '{print $2}'`
-mem=$(( ($totalmem / $cores)* 1024 ))
+## Enable Zram
+modprobe zram num_devices=4
 
-core=0
-while [ $core -lt $cores ]; do
-  dev=$(zramctl -a lz4 -s $mem --find)
-  echo $dev
-  mkswap $dev
-  swapon -p 5 $dev
-  let core=core+1
-done
+## Set LZ4 as compression algorithm
+echo lz4 > /sys/block/zram0/comp_algorithm
+echo lz4 > /sys/block/zram1/comp_algorithm
+echo lz4 > /sys/block/zram2/comp_algorithm
+echo lz4 > /sys/block/zram3/comp_algorithm
+
+##1000MB Total ZRAM
+mem=262100000
+
+##Set size and create swap device
+echo $mem > /sys/block/zram0/disksize
+mkswap /dev/zram0
+swapon -p 0 /dev/zram0
+echo $mem > /sys/block/zram1/disksize
+mkswap /dev/zram1
+swapon -p 0 /dev/zram1
+echo $mem > /sys/block/zram2/disksize
+mkswap /dev/zram2
+swapon -p 0 /dev/zram2
+echo $mem > /sys/block/zram3/disksize
+mkswap /dev/zram3
+swapon -p 0 /dev/zram3
