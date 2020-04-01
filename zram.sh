917431566ae53f2c75ce1900ec674b34b7406369
Alberto <52979095+generaleramon@users.noreply.github.com>
2020-04-01 13:47:43 +0200

    Update zram.sh
    small corrections + reduced the devices to a single big one, as compression streams number is detected automatically

--- zram.sh
+++ zram.sh
@@ -2,9 +2,7 @@
 
 export LANG=C
 
-cores=4
-
-## Disable Zram
+## Disable existing ZRam devices
 swapoff /dev/zram0
 swapoff /dev/zram1
 swapoff /dev/zram2
@@ -24,23 +22,12 @@ modprobe zram num_devices=4
 
 ## Set LZ4 as compression algorithm
 echo lz4 > /sys/block/zram0/comp_algorithm
-echo lz4 > /sys/block/zram1/comp_algorithm
-echo lz4 > /sys/block/zram2/comp_algorithm
-echo lz4 > /sys/block/zram3/comp_algorithm
 
-##1000MB Total ZRAM
-mem=262100000
+##Set 1000MB as Total ZRAM Size
+mem=1048400000
 
 ##Set size and create swap device
+##Max number of compression streams(Cores) is detected automatically
 echo $mem > /sys/block/zram0/disksize
 mkswap /dev/zram0
 swapon -p 0 /dev/zram0
-echo $mem > /sys/block/zram1/disksize
-mkswap /dev/zram1
-swapon -p 0 /dev/zram1
-echo $mem > /sys/block/zram2/disksize
-mkswap /dev/zram2
-swapon -p 0 /dev/zram2
-echo $mem > /sys/block/zram3/disksize
-mkswap /dev/zram3
-swapon -p 0 /dev/zram3
