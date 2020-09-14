2ada035754d538f5feddd1f8a500fee1b939cc84
Alberto <52979095+generaleramon@users.noreply.github.com>
2020-09-14 08:43:26 +0200

    Update compact.sh

--- compact.sh
+++ compact.sh
@@ -1,8 +1,4 @@
-#The compact file is write-only and trigger compaction for
-#allocator zrm uses. The allocator moves some objects so that
-#it could free fragment space.
+#The compact file trigger compaction for allocator zram uses. The allocator moves some objects so that it could free fragment space
 
 echo 1 > /sys/block/zram0/compact
 echo 1 > /sys/block/zram1/compact
-echo 1 > /sys/block/zram2/compact
-echo 1 > /sys/block/zram3/compact
