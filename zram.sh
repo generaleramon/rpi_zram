b9eb2e7f4f69250868c95c572b446cec60c2a77f
Alberto <52979095+generaleramon@users.noreply.github.com>
2024-11-20 14:37:34 +0000

    Update zram.sh

--- zram.sh
+++ zram.sh
@@ -20,19 +20,15 @@ swapoff -a
 ## Enable 2 Zram devices
 modprobe zram num_devices=2
 
-## Set LZ4 and LZ4HC as compression algorithms
+## Set LZ4 as compression algorithms
 echo lz4 > /sys/block/zram0/comp_algorithm
-echo zstd > /sys/block/zram1/comp_algorithm
 
 ##Set 4GB as -Total- ZRAM Size
-mem=2150000000
+mem=4350000000
 
 ##Set size and create swap devices
-##LZ4 as 1th level and LZ4HC as 2th level
+##LZ4 as 1th level compression
 ##Max number of compression streams(Cores) is detected automatically
 echo $mem > /sys/block/zram0/disksize
 mkswap /dev/zram0
 swapon -p 1 /dev/zram0
-echo $mem > /sys/block/zram1/disksize
-mkswap /dev/zram1
-swapon -p 0 /dev/zram1
